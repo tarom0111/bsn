6988b62aa9aa6adbf8ea4846cfc039db839b4142
tarom0111 <77558188+tarom0111@users.noreply.github.com>
2022-05-05 06:28:54 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# BSN
+
+#### installation
+rm -rf BSN
+
+git clone https://github.com/BIL4L-KH4N/BSN
+
+#### For Run
+
+cd BSN
+
+python bsn.py
+
---
+++ bsn.py
@@ -0,0 +1,8 @@
+import os,platform
+os.system('git pull')
+
+bsn=platform.architecture()[0]
+if bsn=="32bit":
+    print('Sorry Update Your Phone...')
+elif bsn=="64bit":
+    __import__("bs").reg()
